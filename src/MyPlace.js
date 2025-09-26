c6c8fd81d8a3ba359ffca53081f1eeb687c5a224
Suffian <suffianakhtar@gmail.com>
2025-09-26 19:32:53 +0500

    Fix path issue in my-place/index.html

--- src/MyPlace.js
+++ src/MyPlace.js
@@ -1,5 +1,10 @@
 import renderMap from './UI/Map';
 
+const newPlaceLinkEl = document.getElementById('new-place-link');
+const basePath = location.origin + location.pathname.replace(/my-place\/index\.html$/, '');
+console.log(basePath);
+newPlaceLinkEl.href = basePath + '/index.html';
+
 const url = new URL(location.href);
 const address = url.searchParams.get('address');
 const lat = url.searchParams.get('lat');
